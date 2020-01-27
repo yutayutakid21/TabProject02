1fd44bee1e7efdec0c1ebab360aac3df8bcaf475
Fujio Nakatsuka <com.nifty.cbm01799>
2020-01-27 19:37:52 +0900

    Initial Commit

---
+++ TabProject02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabProject02
+//
+//  Created by 中塚富士雄 on 2020/01/27.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
