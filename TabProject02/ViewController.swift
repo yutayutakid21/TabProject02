ee9586590505d9c10d23d1dcc0d39ff774563ccf
Fujio Nakatsuka <com.nifty.cbm01799>
2020-01-30 07:11:23 +0900

    ルーレットをViewControllerに入れて。。。。と、止まらない？！item Tabを押すと同じguruguru画面が2度目の登場！なぜ？

--- TabProject02/ViewController.swift
+++ TabProject02/ViewController.swift
@@ -10,12 +10,42 @@ import UIKit
 import JBTabBarAnimation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    var imageArray = ["0","1","2","3","4","5"]
+    var count = Int()
+    var timer = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    count = 0
+
     }
 
+    @IBAction func start(_ sender: Any) {
+        
+         timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(guruguru),userInfo: nil, repeats: true)
+        
+        
+    }
+    @objc func guruguru(){
+           
+           count = count + 1
+           
+           if count > 5{
+           count = 0
+           imageView.image = UIImage(named: imageArray[count])
+           }else{
+               
+            imageView.image = UIImage(named: imageArray[count])
+            }
+    }
 
+   
+        func stop(_ sender: Any) {
+                   
+                   timer.invalidate()
+       
+   
+    }
+     
 }
-
